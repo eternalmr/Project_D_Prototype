9e1df0ca067eaffb0985a31b3907c7d4060c5c6c
cr <eternal_mr@qq.com>
2019-12-13 10:20:15 +0800

    test lbbroker.cpp passed compiled

--- test2/test.cpp
+++ test2/test.cpp
@@ -1,80 +1,179 @@
+//  Least-recently used (LRU) queue device
+//  Clients and workers are shown here in-process
+//
+// Olivier Chamoux <olivier.chamoux@fr.thalesgroup.com>
 
-//#include <iostream>
-//#include <thread>
-//#include <windows.h>
-//
-//void msg()
-//{
-//	MessageBoxA(0, "对话框内容", "对话框标题", 0);//弹出对话框
-//}
-//
-//void main()
-//{
-//	auto n = std::thread::hardware_concurrency();//检测CPU有多少个核心
-//	std::cout << n << std::endl;
-//
-//	std::cout << "thread=" << std::this_thread::get_id() << std::endl;//获取当前线程编号
-//
-//	std::thread thread1(msg);//创建多线程
-//	std::thread thread2(msg);
-//
-//	thread1.join();//开始执行，同时弹出2个对话框
-//	thread2.join();
-//
-//	system("pause");
-//}
+#pragma warning(disable:4996)
 
+#include "zhelpers.hpp"
+#include <pthread.h>
+#include <queue>
 
+//  Basic request-reply client using REQ socket
+//
+static void *
+client_thread(void *arg) {
+	zmq::context_t context(1);
+	zmq::socket_t client(context, ZMQ_REQ);
 
-#include <iostream>
-#include <thread>
+#if (defined (WIN32))
+	s_set_id(client, (intptr_t)arg);
+	client.connect("tcp://localhost:5672"); // frontend
+#else
+	s_set_id(client); // Set a printable identity
+	client.connect("ipc://frontend.ipc");
+#endif
 
-void fun1()
-{
-	std::cout << "this is fun1"<< std::endl;
+	//  Send request, get reply
+	s_send(client, "HELLO");
+	std::string reply = s_recv(client);
+	std::cout << "Client: " << reply << std::endl;
+	return (NULL);
 }
 
-void fun2()
-{
-	std::cout << "this is fun2" << std::endl;
+//  Worker using REQ socket to do LRU routing
+//
+static void *
+worker_thread(void *arg) {
+	zmq::context_t context(1);
+	zmq::socket_t worker(context, ZMQ_REQ);
+
+#if (defined (WIN32))
+	s_set_id(worker, (intptr_t)arg);
+	worker.connect("tcp://localhost:5673"); // backend
+#else
+	s_set_id(worker);
+	worker.connect("ipc://backend.ipc");
+#endif
+
+	//  Tell backend we're ready for work
+	s_send(worker, "READY");
+
+	while (1) {
+		//  Read and save all frames until we get an empty frame
+		//  In this example there is only 1 but it could be more
+		std::string address = s_recv(worker);
+		{
+			std::string empty = s_recv(worker);
+			assert(empty.size() == 0);
+		}
+
+		//  Get request, send reply
+		std::string request = s_recv(worker);
+		std::cout << "Worker: " << request << std::endl;
+
+		s_sendmore(worker, address);
+		s_sendmore(worker, "");
+		s_send(worker, "OK");
+	}
+	return (NULL);
 }
 
-void fun3()
+int main(int argc, char *argv[])
 {
-	std::cout << "this is fun3" << std::endl;
-}
 
-void fun4()
-{
-	std::cout << "this is fun4" << std::endl;
-}
+	//  Prepare our context and sockets
+	zmq::context_t context(1);
+	zmq::socket_t frontend(context, ZMQ_ROUTER);
+	zmq::socket_t backend(context, ZMQ_ROUTER);
 
-void fun5()
-{
-	std::cout << "this is fun5" << std::endl;
-}
+#if (defined (WIN32))
+	frontend.bind("tcp://*:5672"); // frontend
+	backend.bind("tcp://*:5673"); // backend
+#else
+	frontend.bind("ipc://frontend.ipc");
+	backend.bind("ipc://backend.ipc");
+#endif
 
+	int client_nbr;
+	for (client_nbr = 0; client_nbr < 10; client_nbr++) {
+		pthread_t client;
+		pthread_create(&client, NULL, client_thread, (void *)(intptr_t)client_nbr);
+	}
+	int worker_nbr;
+	for (worker_nbr = 0; worker_nbr < 3; worker_nbr++) {
+		pthread_t worker;
+		pthread_create(&worker, NULL, worker_thread, (void *)(intptr_t)worker_nbr);
+	}
+	//  Logic of LRU loop
+	//  - Poll backend always, frontend only if 1+ worker ready
+	//  - If worker replies, queue worker as ready and forward reply
+	//    to client if necessary
+	//  - If client requests, pop next worker and send request to it
+	//
+	//  A very simple queue structure with known max size
+	std::queue<std::string> worker_queue;
 
+	while (1) {
 
-int main()
-{
-	std::thread t1(fun1);
-	std::thread t2(fun2);
-	std::thread t3(fun3);
-	std::thread t4(fun4);
-	std::thread t5(fun5);
+		//  Initialize poll set
+		zmq::pollitem_t items[] = {
+			//  Always poll for worker activity on backend
+				{ backend, 0, ZMQ_POLLIN, 0 },
+				//  Poll front-end only if we have available workers
+				{ frontend, 0, ZMQ_POLLIN, 0 }
+		};
+		if (worker_queue.size())
+			zmq::poll(&items[0], 2, -1);
+		else
+			zmq::poll(&items[0], 1, -1);
 
-	
-	
+		//  Handle worker activity on backend
+		if (items[0].revents & ZMQ_POLLIN) {
 
-	t1.join();
-	t2.join();
-	t3.join();
-	t4.join();
-	t5.join();
+			//  Queue worker address for LRU routing
+			worker_queue.push(s_recv(backend));
 
+			{
+				//  Second frame is empty
+				std::string empty = s_recv(backend);
+				assert(empty.size() == 0);
+			}
+
+			//  Third frame is READY or else a client reply address
+			std::string client_addr = s_recv(backend);
+
+			//  If client reply, send rest back to frontend
+			if (client_addr.compare("READY") != 0) {
+
+				{
+					std::string empty = s_recv(backend);
+					assert(empty.size() == 0);
+				}
+
+				std::string reply = s_recv(backend);
+				s_sendmore(frontend, client_addr);
+				s_sendmore(frontend, "");
+				s_send(frontend, reply);
+
+				if (--client_nbr == 0)
+					break;
+			}
+		}
+		if (items[1].revents & ZMQ_POLLIN) {
+
+			//  Now get next client request, route to LRU worker
+			//  Client request is [address][empty][request]
+			std::string client_addr = s_recv(frontend);
+
+			{
+				std::string empty = s_recv(frontend);
+				assert(empty.size() == 0);
+			}
+
+			std::string request = s_recv(frontend);
+
+			std::string worker_addr = worker_queue.front();//worker_queue [0];
+			worker_queue.pop();
+
+			s_sendmore(backend, worker_addr);
+			s_sendmore(backend, "");
+			s_sendmore(backend, client_addr);
+			s_sendmore(backend, "");
+			s_send(backend, request);
+		}
+	}
 
 	system("pause");
-
 	return 0;
 }
