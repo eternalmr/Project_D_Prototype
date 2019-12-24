23c25148773ade2151365451f379d614e9c67747
cr <eternal_mr@qq.com>
2019-12-24 21:59:29 +0800

    delete some comment

--- TestFunction/test1.cpp
+++ TestFunction/test1.cpp
@@ -1,12 +1,7 @@
-// test1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-////#include "pch.h"
-//#include <iostream>
 
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
 
+#include "catch.hpp"
 #include "myfun.hpp"
 
 
