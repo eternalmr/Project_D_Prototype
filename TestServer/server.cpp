83c646a9553602967c9ead2e5b4d82fded979268
cr <eternal_mr@qq.com>
2019-12-27 14:40:36 +0800

    using a new task assign function

--- TestServer/server.cpp
+++ TestServer/server.cpp
@@ -8,47 +8,30 @@
 #include "zhelpers.hpp"
 #include <thread>
 
-int GenerateTasks()
-{
+int assign_tasks() {
 	zmq::context_t context(1);
-	zmq::socket_t requester(context, ZMQ_DEALER);
-	requester.connect("tcp://192.168.100.239:5559");
+	zmq::socket_t server(context, ZMQ_REP);
+	server.bind("tcp://192.168.100.239:5560");
 
-	for (int request = 0; request < 20; request++) {
+	// TODO : start heartbeat and collector thread
 
-		s_sendmore(requester, "");
-		std::string str = std::to_string(request);
-		s_send(requester, str);
+	// TODO : create task pool
 
-		std::string result = s_recv(requester);
+	// TODO : create worker queue
 
-		std::cout << "Result of request[" << request
-			<< "]: " << result << std::endl;
+	int workload = 0;
+	while (true) { // while tasks not finished
+		// TODO : a_free_worker = get_free_worker(workers queue)
 
-		if (atoi(result.c_str()) == -1) {
-			break;
-		}
+		// TODO : a_undo_task = get_undo_task(tasks pool)
+
+		// TODO : assign_task_to(a_free_worker, a_undo_task)
+
+		std::string reply = s_recv(server);
+		std::cout << "Receive request: " << reply << std::endl;
+		s_send(server, std::to_string(workload));
+		workload++;
 	}
-
-	std::cout << "Simulation finished!" << std::endl;
-	return 0;
-}
-
-int AssignTasks()
-{
-	//  Prepare our context and sockets
-	zmq::context_t context(1);
-	zmq::socket_t frontend(context, ZMQ_ROUTER);
-	zmq::socket_t backend(context, ZMQ_DEALER);
-
-	frontend.bind("tcp://192.168.100.239:5559");
-	backend.bind("tcp://192.168.100.239:5560");
-
-	zmq::proxy(frontend, backend, NULL);
-
-	frontend.close();
-	backend.close();
-	context.close();
 
 	return 0;
 }
@@ -60,20 +43,10 @@ int main()
 	zmq::socket_t socket(context, ZMQ_PUB);
 	socket.bind("tcp://192.168.100.239:5555");
 
-	//// Send test message
-	//s_send(socket, "Hello");
-	//std::cout << "Say Hello to client" << std::endl;
-
-	////  Get the reply.
-	//std::string replys = s_recv(socket);
-	//std::cout << "Received [" << replys << "] from client" << std::endl;
-
-	// 启动任务线程和代理线程
-	std::thread task_thread(GenerateTasks);
-	std::thread proxy_thread(AssignTasks);
+	// 启动任务线程
+	std::thread task_thread(assign_tasks);
 
 	char command;
-	std::string replys;
 	while (true) {
 		std::cout << "Please input your command: ";
 		std::cin >> command;
@@ -92,17 +65,6 @@ int main()
 			std::cout << "Wrong command!" << std::endl;
 			continue;
 		}
-
-		//replys = s_recv(socket);
-		//if (replys == "finished") {
-		//	std::cout << "Simulation finished!" << std::endl;
-		//	break;
-		//} else if (replys == "interrupt") {
-		//	std::cout << "Simulation interrupted!" << std::endl;
-		//	break;
-		//} else {
-		//	std::cout << "Received [" << replys << "] from client" << std::endl;
-		//}
 	}
 
 	system("pause");
