94c3ddda7042b5a171ac53257c83e2ecceccbe51
cr <eternal_mr@qq.com>
2019-12-31 16:57:44 +0800

    add a parameter client_id to send_heartbeat()

--- TestClient/client.cpp
+++ TestClient/client.cpp
@@ -29,14 +29,15 @@ std::vector<std::string> s_split(const std::string& in,
 	};
 }
 
-void send_heartbeat(zmq::context_t &context)
+void send_heartbeat(zmq::context_t &context, unsigned int client_id)
 {
 	zmq::socket_t heartbeat_sender(context, ZMQ_PUSH);
 	heartbeat_sender.connect("tcp://127.0.0.1:1217");
 
+	std::string signal = "HEARTBEAT_" + std::to_string(client_id);
 	// send heartbeat at regular interval
 	while (true) {
-		s_send(heartbeat_sender, "HEARTBEAT_1");
+		s_send(heartbeat_sender, signal);
 		std::this_thread::sleep_for(std::chrono::milliseconds(HEARTBEAT_INTERVAL));
 	}
 }
@@ -46,7 +47,7 @@ int main()
 	zmq::context_t context(1);
 	zmq::socket_t test_socket(context, ZMQ_PULL);
 
-	std::thread heartbeat_thread(send_heartbeat, std::ref(context));
+	std::thread heartbeat_thread(send_heartbeat, std::ref(context), 1);
 
 	test_socket.bind("tcp://127.0.0.1:1217");
 	//should receive 10 heartbeat in specific time interval
